e92c28384f6597791ee630ac28274123a39c1ac8
ValentinaPucci <135621497+ValentinaPucci@users.noreply.github.com>
2024-03-23 10:52:55 +0100

    DHR_kidneys_315to339

---
+++ CODE/paths.py
@@ -0,0 +1,10 @@
+original_data_path = '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/dataset' # Path to the original ANHIR dataset 
+csv_path = '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/dataset_medium.csv' # Path to the ANHIR .csv file
+
+parsed_data_path = '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/dataset_parsed' # Path to the ANHIR dataset (parsed, in the .mha format)
+
+training_path = '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/step4/training' # Path to the training dataset (nonrigid)
+validation_path = '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/step4/evaluation' # Path to the validation dataset (nonrigid)
+
+models_path =  '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/models' # Path to the folder where models should be saved
+figures_path = '/Volumes/TOSHIBA_EXT/NECST/DeepHistReg/figures' # Path to the folder where figures should be saved
